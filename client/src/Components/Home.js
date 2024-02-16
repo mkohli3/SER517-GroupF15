956f95b06cd52be369f42dc1ebe6f138a2445595
mkohli3 <112653716+mkohli3@users.noreply.github.com>
2024-02-16 15:25:18 -0700

    Routes on click new sheet

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,14 +1,18 @@
 import './Home.css';
 import ASULogo from '../utils/ASU_logo.png';
+import { useNavigate } from 'react-router-dom';
 export const Home = () =>
 {
+    const navigate = useNavigate();
+    
     const handleExistingSheet = () =>
     {
 
     };
     const handleNewSheet = () =>
     {
-
+    // Navigate to the "/new" route when creating a new sheet
+    navigate('/new');
     };
     return(
         <div className="home">
