8ece025ba7328ba54b7ebb05885d17a4802a1d80
Ajinkyapise <ajinkyapise@gmail.com>
2024-02-26 17:54:39 -0700

    Made changes in Main screen

--- client/src/Components/MainScreen.js
+++ client/src/Components/MainScreen.js
@@ -1,24 +1,52 @@
 import React from 'react';
-import {useLocation} from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { Button, Container, CssBaseline, Typography, TextField, FormGroup, FormControlLabel, Checkbox, Paper } from '@mui/material';
 
-const MainScreen = ( ) => {
-
-    const criteriaList = useLocation().state.criteriaList;
+const MainScreen = () => {
+  const criteriaList = useLocation().state.criteriaList;
+  
 
   return (
     <div>
       <h2>Grading Criteria Display</h2>
-      <div>
-        {/* Display the grading criteria in a row */}
-        {criteriaList && criteriaList.map((criteria, index) => (
-          <div key={index}>
-            <p>Criteria Name: {criteria.criteria}</p>
-            <p>Points: {criteria.points}</p>
-            <p>Group Criteria: {criteria.group ? 'Yes' : 'No'}</p>
-            <p>Individual Criteria: {criteria.individual ? 'Yes' : 'No'}</p>
-            <hr />
-          </div>
-        ))}
+      <table>
+        <thead>
+          <tr>
+            <th>Criteria Name</th>
+            <th>Points</th>
+            <th>Group Criteria</th>
+            <th>Individual Criteria</th>
+          </tr>
+        </thead>
+        <tbody>
+          {criteriaList &&
+            criteriaList.map((criteria, index) => (
+              <tr key={index}>
+                <td>{criteria.criteria}</td>
+                <td>{criteria.points}</td>
+                <td>{criteria.group ? 'Yes' : 'No'}</td>
+                <td>{criteria.individual ? 'Yes' : 'No'}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+
+              
+      <div style={{ position: 'fixed', bottom: '50px', left: '50%', transform: 'translateX(-50%)', margin: '5px 0' }}>
+        <Typography component="h2" variant="h6" style={{ fontSize: '18px', fontWeight: 'normal', color: '#800000' }}>
+          Enter Student Details Next
+        </Typography>
+
+        <Button
+          type="button"
+          fullWidth
+          variant="contained"
+          color="primary"
+          // onClick={handleNextButtonClick}
+          style={{ margin: '5px 0' }}
+        >
+          Next
+        </Button>
       </div>
     </div>
   );
