91c87861d16e86f9e7fc2184b39ec3c29dab247c
preetshah69 <pshah82@asu.edu>
2024-03-01 17:45:08 -0700

    Routed correctly

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,23 +1,24 @@
+import React from 'react';
 import './Home.css';
 import ASULogo from '../utils/ASU_logo.png';
-export const Home = () =>
-{
-    const handleExistingSheet = () =>
-    {
+import { useNavigate } from 'react-router-dom';
+import App from '../App';
+export const Home = () => {
+    const navigate = useNavigate();
 
+    const handleExistingSheet = () => {
+        navigate('/grading-criteria-upload');
     };
-    const handleNewSheet = () =>
-    {
 
+    const handleNewSheet = () => {
+        navigate('/manual-entry');
     };
-    return(
+
+    return (
         <div className="home">
             <img src={ASULogo} alt='ASU-LOGO' />
             <button type='button' onClick={handleExistingSheet}>Open existing sheet</button>
             <button type='button' onClick={handleNewSheet}>Create a new sheet</button>
-        
-
         </div>
-
     );
-}
+};
