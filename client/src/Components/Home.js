2ce32c3874a960a18d54689041a3626cfe92fd82
Ajinkyapise <ajinkyapise@gmail.com>
2024-03-06 20:14:34 -0700

    Reuploading Sprint3 changes

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,24 +1,27 @@
-import React from 'react';
 import './Home.css';
 import ASULogo from '../utils/ASU_logo.png';
 import { useNavigate } from 'react-router-dom';
-import App from '../App';
-export const Home = () => {
+export const Home = () =>
+{
     const navigate = useNavigate();
+    
+    const handleExistingSheet = () =>
+    {
 
-    const handleExistingSheet = () => {
-        navigate('/grading-criteria-upload');
     };
-
-    const handleNewSheet = () => {
-        navigate('/manual-entry');
+    const handleNewSheet = () =>
+    {
+    // Navigate to the "/new" route when creating a new sheet
+    navigate('/new');
     };
-
-    return (
+    return(
         <div className="home">
             <img src={ASULogo} alt='ASU-LOGO' />
             <button type='button' onClick={handleExistingSheet}>Open existing sheet</button>
             <button type='button' onClick={handleNewSheet}>Create a new sheet</button>
+        
+
         </div>
+
     );
-};
+}
