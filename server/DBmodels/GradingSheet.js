67a32f2ea8a77cec76c1a4a49a3b2436b805eba7
Heet Punjawat <hpunjawa@asu.edu>
2024-03-12 23:14:42 -0700

    Update GradingSheet.js

--- server/DBmodels/GradingSheet.js
+++ server/DBmodels/GradingSheet.js
@@ -6,16 +6,16 @@ const mongoose = require('mongoose');
 const gradingCriteriaSchema = new mongoose.Schema({
   criteriaName: String,
   points: Number,
-  applicableTo: { type: String, enum: ['group', 'individual', 'both'] },
-  hiddenComments: Boolean,
-}, { _id: false });
+  groupCriteria: Boolean,
+  individualCriteria: Boolean
+}, { _id: false }); // Keeping _id: false because we don't need separate IDs for subdocuments for now.
 
 const gradingSheetSchema = new mongoose.Schema({
   title: String,
   serialNo: Number,
   ASUriteId: String,
   StudentName: String,
-  gradingCriteria: [gradingCriteriaSchema] 
+  gradingCriteria: [gradingCriteriaSchema] // Embeds the grading criteria directly into each grading sheet.
 });
 
 module.exports = mongoose.model('GradingSheet', gradingSheetSchema);
