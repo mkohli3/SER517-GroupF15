469bce0743789d4fedccad867640b558bd93533d
Manmeet-123 <tnc01997@gmail.com>
2024-03-28 15:18:38 -0700

    Comment Display corresponding to deductions

--- client/src/Components/ManualGradingCriteriaUpload.js
+++ client/src/Components/ManualGradingCriteriaUpload.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Container, CssBaseline, Typography, TextField, Paper, IconButton } from '@mui/material';
+import { Button, Container, CssBaseline, Typography, TextField, Paper, IconButton, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { useNavigate } from 'react-router-dom';
 import './ManualGradingCriteriaUpload.css';
@@ -40,7 +40,7 @@ function ManualGradingCriteriaUpload() {
   const addCriteria = () => {
     setCriteriaList([
       ...criteriaList,
-      { criteria: '', points: '', deductions: [] },
+      { criteria: '', points: '', deductions: [], type: 'Individual' }, // Default to Individual
     ]);
   };
 
@@ -57,7 +57,7 @@ function ManualGradingCriteriaUpload() {
   };
 
   const handleSaveButtonClick = () => {
-    console.log(criteriaList)
+    console.log(criteriaList);
     navigate('/main-screen', { state: { criteriaList: criteriaList } });
   };
 
@@ -66,53 +66,63 @@ function ManualGradingCriteriaUpload() {
       <Container key={criteriaIndex} component="main"  className="criteria-container">
         <CssBaseline />
         
-          <TextField
-            label="Criteria Name"
-            variant="outlined"
-            fullWidth
-            InputProps={{ style: { color: '#8C1D40', backgroundColor: 'goldenrod' } }} // Add color and background to the input
-                InputLabelProps={{ style: { color: '#8C1D40' } }}
-            value={criteria.criteria}
-            onChange={(e) => updateCriteria(criteriaIndex, 'criteria', e.target.value)}
-          />
-          <TextField
-            label="Points"
-            type="number"
-            variant="outlined"
-            fullWidth
-            InputProps={{ style: { color: '#8C1D40', backgroundColor: 'goldenrod' } }} // Add color and background to the input
-                InputLabelProps={{ style: { color: '#8C1D40' } }}
-            value={criteria.points}
-            onChange={(e) => updateCriteria(criteriaIndex, 'points', e.target.value)}
-          />
-          
-          {criteria.deductions.map((deduction, deductionIndex) => (
-            <div key={deductionIndex} className="deduction-container">
-              <TextField
-                label={`Deduction ${deductionIndex + 1} Points`}
-                type="number"
-                variant="outlined"InputProps={{ style: { color: '#8C1D40', backgroundColor: 'goldenrod' } }} // Add color and background to the input
-                InputLabelProps={{ style: { color: '#8C1D40' } }}
-                
-                value={deduction.points}
-                onChange={(e) => updateDeductions(criteriaIndex,deductionIndex, 'points', e.target.value)}
-              />
-              <TextField
-                label={`Deduction ${deductionIndex + 1} Comment`}
-                variant="outlined"
-                fullWidth
-                InputProps={{ style: { color: '#8C1D40', backgroundColor: 'goldenrod' } }} // Add color and background to the input
-                InputLabelProps={{ style: { color: '#8C1D40' } }}
-
-                value={deduction.comment}
-                onChange={(e) => updateDeductions(criteriaIndex, deductionIndex, 'comment', e.target.value, deductionIndex)}
-              />
-              <IconButton className="delete-icon" onClick={() => removeDeduction(criteriaIndex, deductionIndex)}>
-                <DeleteIcon />
-              </IconButton>
-            </div>
-          ))}
-          <button className="add-deduction-button" onClick={() => addDeduction(criteriaIndex)}>Add Deduction</button>
+        <TextField
+          label="Criteria Name"
+          variant="outlined"
+          fullWidth
+          InputProps={{ style: { color: '#8C1D40', backgroundColor: 'goldenrod' } }} 
+          InputLabelProps={{ style: { color: '#8C1D40' } }}
+          value={criteria.criteria}
+          onChange={(e) => updateCriteria(criteriaIndex, 'criteria', e.target.value)}
+        />
+        <TextField
+          label="Points"
+          type="number"
+          variant="outlined"
+          fullWidth
+          InputProps={{ style: { color: '#8C1D40', backgroundColor: 'goldenrod' } }} 
+          InputLabelProps={{ style: { color: '#8C1D40' } }}
+          value={criteria.points}
+          onChange={(e) => updateCriteria(criteriaIndex, 'points', e.target.value)}
+        />
+        <FormControl fullWidth variant="outlined">
+          <InputLabel>Type</InputLabel>
+          <Select
+            value={criteria.type}
+            onChange={(e) => updateCriteria(criteriaIndex, 'type', e.target.value)}
+            label="Type"
+          >
+            <MenuItem value="Individual">Individual</MenuItem>
+            <MenuItem value="Group">Group</MenuItem>
+          </Select>
+        </FormControl>
+        
+        {criteria.deductions.map((deduction, deductionIndex) => (
+          <div key={deductionIndex} className="deduction-container">
+            <TextField
+              label={`Deduction ${deductionIndex + 1} Points`}
+              type="number"
+              variant="outlined"
+              InputProps={{ style: { color: '#8C1D40', backgroundColor: 'goldenrod' } }} 
+              InputLabelProps={{ style: { color: '#8C1D40' } }}
+              value={deduction.points}
+              onChange={(e) => updateDeductions(criteriaIndex,deductionIndex, 'points', e.target.value)}
+            />
+            <TextField
+              label={`Deduction ${deductionIndex + 1} Comment`}
+              variant="outlined"
+              fullWidth
+              InputProps={{ style: { color: '#8C1D40', backgroundColor: 'goldenrod' } }} 
+              InputLabelProps={{ style: { color: '#8C1D40' } }}
+              value={deduction.comment}
+              onChange={(e) => updateDeductions(criteriaIndex, deductionIndex, 'comment', e.target.value)}
+            />
+            <IconButton className="delete-icon" onClick={() => removeDeduction(criteriaIndex, deductionIndex)}>
+              <DeleteIcon />
+            </IconButton>
+          </div>
+        ))}
+        <button className="add-deduction-button" onClick={() => addDeduction(criteriaIndex)}>Add Deduction</button>
       </Container>
     ));
   };
@@ -120,17 +130,15 @@ function ManualGradingCriteriaUpload() {
   return (
     <div className="manual-grading-criteria-upload">
       <div>
-
-
-<div style={{ position: 'fixed', top:"0px", left: '50%', width:"100vw", transform: 'translateX(-50%)', zIndex: '9999', backgroundColor:"goldenrod" }}>
-<h1 style={{  left: '0px',top:"0px", width: '100%', backgroundColor: '#8C1D40', color: 'goldenrod', padding: '10px', borderRadius: '5px', textAlign: 'center', marginBottom:'0px', position:"relative", top:"-20px" }}>Add Grading Criteria Below</h1>
-<div style={{display:"flex", justifyContent:"center"}}>
-  <button onClick={handleSaveButtonClick}>Save</button>
-  <button onClick={addCriteria}>Add New Criteria</button>
-  </div>
-</div>
-</div>
-    <div style={{ marginTop: '150px' }}>{renderCriteriaInputs()}</div>
+        <div style={{ position: 'fixed', top:"0px", left: '50%', width:"100vw", transform: 'translateX(-50%)', zIndex: '9999', backgroundColor:"goldenrod" }}>
+          <h1 style={{  left: '0px',top:"0px", width: '100%', backgroundColor: '#8C1D40', color: 'goldenrod', padding: '10px', borderRadius: '5px', textAlign: 'center', marginBottom:'0px', position:"relative", top:"-20px" }}>Add Grading Criteria Below</h1>
+          <div style={{display:"flex", justifyContent:"center"}}>
+            <button onClick={handleSaveButtonClick}>Save</button>
+            <button onClick={addCriteria}>Add New Criteria</button>
+          </div>
+        </div>
+      </div>
+      <div style={{ marginTop: '150px' }}>{renderCriteriaInputs()}</div>
     </div>
   );
 }
